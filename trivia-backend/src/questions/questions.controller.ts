af11a9b2e1ac87c899a5c2cf851167acb3adb05e
alexxxmf <amoro@alumni.unav.es>
2020-01-02 19:04:28 +0100

    Added missing endpoints plus adding some todos to readme

--- trivia-backend/src/questions/questions.controller.ts
+++ trivia-backend/src/questions/questions.controller.ts
@@ -13,6 +13,8 @@ import { QuestionsService } from './questions.service';
 import { Question, Category } from './question.entity';
 import { CreateQuestionDto } from './dto/create-question.dto';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { get } from 'https';
+import { UpdateQuestionDto } from './dto/update-question.dto';
 
 @Controller('questions')
 export class QuestionsController {
@@ -23,11 +25,29 @@ export class QuestionsController {
     return await this.questionsService.getQuestions();
   }
 
+  @Get()
+  async getQuestionById(@Param('id') id: string): Promise<Question> {
+    return this.getQuestionById(id);
+  }
+
+  @Delete()
+  async deleteQuestionById(@Param('id') id: string): Promise<void> {
+    return this.deleteQuestionById(id);
+  }
+
   @Post()
   async createQuestion(
     @Body() createQuestionDto: CreateQuestionDto,
   ): Promise<Question> {
     return this.questionsService.createQuestion(createQuestionDto);
+  }
+
+  @Patch()
+  async updateQuestion(
+    @Param('id') id: string,
+    @Body() updateQuestionDto: UpdateQuestionDto,
+  ): Promise<Question> {
+    return this.questionsService.updateQuestionById(id, updateQuestionDto);
   }
 
   @Get('/categories')
--- trivia-backend/src/questions/questions.service.ts
+++ trivia-backend/src/questions/questions.service.ts
@@ -2,12 +2,14 @@ import {
   Injectable,
   NotFoundException,
   ConflictException,
+  Param,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { QuestionRepository, CategoryRepository } from './question.repository';
 import { Question, Category } from './question.entity';
 import { CreateQuestionDto } from './dto/create-question.dto';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateQuestionDto } from './dto/update-question.dto';
 
 @Injectable()
 export class QuestionsService {
@@ -22,10 +24,54 @@ export class QuestionsService {
     return await this.questionRepository.getQuestions();
   }
 
+  async getQuestionById(@Param('id') id: string): Promise<Question> {
+    const found = await this.questionRepository.findOne({ where: { id } });
+    if (found) {
+      return found;
+    } else {
+      throw new NotFoundException(`Question with id "${id}" not found`);
+    }
+  }
+
   async createQuestion(
     createQuestionDto: CreateQuestionDto,
   ): Promise<Question> {
     return await this.questionRepository.createQuestion(createQuestionDto);
+  }
+
+  async updateQuestionById(
+    id: string,
+    updateQuestionDto: UpdateQuestionDto,
+  ): Promise<Question> {
+    const questionInstance = await this.getQuestionById(id);
+
+    const {
+      type,
+      hint,
+      question,
+      correctAnswer,
+      incorrectAnswers,
+      difficulty,
+    } = updateQuestionDto;
+
+    questionInstance.type = type;
+    questionInstance.hint = hint;
+    questionInstance.question = question;
+    questionInstance.correctAnswer = correctAnswer;
+    // TODO: this is garbage, move this login into entity so we have a central place for serializing
+    questionInstance.incorrectAnswers = JSON.stringify(incorrectAnswers);
+    questionInstance.difficulty = difficulty;
+
+    questionInstance.save();
+
+    return questionInstance;
+  }
+
+  async deleteQuestionById(id: string): Promise<void> {
+    const result = await this.questionRepository.delete({ id });
+    if (result.affected === 0) {
+      throw new NotFoundException(`Task with ID ${id} not found`);
+    }
   }
 
   async getCategories(): Promise<Category[]> {
@@ -57,7 +103,11 @@ export class QuestionsService {
 
   async deleteCategory(id: string): Promise<void> {
     try {
-      await this.categoryRepository.delete({ id });
+      const result = await this.categoryRepository.delete({ id });
+
+      if (result.affected === 0) {
+        throw new NotFoundException(`Task with ID ${id} not found`);
+      }
     } catch (error) {
       if (error.code === '23503') {
         throw new ConflictException(
